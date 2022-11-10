e940a8d225e84f0cd0f50276f6f3221dcf76f198
Dylangroome <dylangroome13@gmail.com>
2022-11-10 06:03:25 +0000

    Refactor uodate worksheet

--- run.py
+++ run.py
@@ -73,6 +73,20 @@ def update_surplus_worksheet(data):
     print('Surplus worksheet updated sucessfully\n')
 
 
+def update_worksheet(data, worksheet):
+    """
+    Recieves a list of integers to be inserted into the worksheet
+    Update the relevant worksheet with the data provided
+    """
+
+    print(f'Updating {worksheet} worksheet...\n')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f'{worksheet} worksheet updated successfully\n')
+
+
+
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -100,9 +114,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_surplus_worksheet(surplus)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
 
 print("Welcome to Love Sandwiches Data Automation")
